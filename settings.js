6ed81ca4417f7d9de8573d2069de530bc1aebb26
rashmikagayashan <108579138+rashmikagayashan@users.noreply.github.com>
2022-07-02 10:18:02 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,17 +19,17 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
+global.owner = ['+94702509556 ']
+global.premium = ['+94702509556']
+global.ownernomer = '+94702509556'
+global.ownername = '🐦Gayashan'
+global.botname = 'official.gaya'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
+global.myweb = 'https:https://www.youtube.com/channel/UCOPgbgIAIkw0vqGhKR7c0IQ/featured
+global.packname = 'Gayashan.BOT'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
